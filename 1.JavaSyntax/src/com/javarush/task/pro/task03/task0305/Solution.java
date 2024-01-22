a462a8d9131aece06caf5e822c96d11496be3218
kornelius26 <gavrilovevgeny1126@gmail.com>
2024-01-22 15:32:16 +0300

    Add Solution JavaSyntax Task0306

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -8,6 +8,23 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count1 = sc.nextInt();
+        int count2 = sc.nextInt();
+        int count3 = sc.nextInt();
+        if ((count1 == count2) && (count3 == count1)) {
+            System.out.println(count1 + " " + count2 + " " + count3);
+        } else if (count1 == count2)  {
+            System.out.println(count1 + " " + count2);
+
+        } else if(count1 == count3) {
+            System.out.println(count1 + " " + count3);
+
+        } else if (count2 == count3) {
+            System.out.println(count2 + " " + count3);
+
+        }
+
         //напишите тут ваш код
     }
 }
