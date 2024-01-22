1f177ec4eebb4c91f43208abde989cf9bf23a1a4
kornelius26 <gavrilovevgeny1126@gmail.com>
2024-01-22 13:33:44 +0300

    Add Solution JavaSyntax Task0103

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int age = 3126;
+        int years = 8;
+        int now = age - years;
+        System.out.println(now);
+        //напишите тут ваш код
+    }
+}
