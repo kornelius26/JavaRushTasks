141607b00941a30780d0260afcbc27c277fd6a04
kornelius26 <gavrilovevgeny1126@gmail.com>
2024-01-26 13:13:11 +0300

    Add Solution JavaSyntax Task0403

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0403/Solution.java
@@ -1,31 +1,25 @@
 package com.javarush.task.pro.task04.task0403;
 
-import java.util.Scanner;
-
-/* 
-Суммирование
-*/
-
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        boolean isExit = false;
-        while(!isExit) {
-            if(scanner.hasNextInt()) {
-                int count = scanner.nextInt();
-                sum = sum + count;
+        int n = 0;
 
-            }else if(scanner.hasNextLine()) {
-                String str = scanner.nextLine();
-                if (str.equals("ENTER")) {
-                    isExit = true;
-                }
+        while (n < 5)
+        {
+
+            int m = 0;
+
+            while (m < 10)
+            {
+                System.out.print("Q");
+                m++;
             }
-        }
-        System.out.println(sum);
 
- //напишите тут ваш код
+            System.out.println();
+
+            n++;
+
+        }
 
     }
 }
