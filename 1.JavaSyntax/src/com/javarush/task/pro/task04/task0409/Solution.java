d12fcab402f9e70a9a05b222cafbb4cd1c25282f
kornelius26 <gavrilovevgeny1126@gmail.com>
2024-02-05 16:18:34 +0300

    Add Solution JavaSyntax Task0409

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0409/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0409/Solution.java
@@ -8,18 +8,15 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        int x = 1;
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()) {
-            int count1 = scanner.nextInt();
-            int count2 = scanner.nextInt();
-            if (count1 < count2) {
-                System.out.println(count1);
+        Scanner console = new Scanner(System.in);
 
-            }else {
-                System.out.println(count2);
+        int min = Integer.MAX_VALUE;
+        while (console.hasNextInt()) {
+            int x = console.nextInt();
+            if (x < min) {
+                min = x;
             }
         }
-
+        System.out.println(min);
     }
 }
